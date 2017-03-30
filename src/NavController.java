8a801ccbfb7794dfc4ee2921561d14fcc72e1a4d
pxk5145 <pxk5145@IST203-102.win.psu.edu>
2017-03-30 15:56:21 -0400

    grid

--- src/NavController.java
+++ src/NavController.java
@@ -1,4 +1,5 @@
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
@@ -388,7 +389,7 @@ public class NavController {
             } else if (c_view.getSpecial().isSelected() == true) {
                 c_view.getPassword().setText(generate.GenSpecial(10));
             } else {
-                c_view.getPassword().setText(generate.GenPass(10));
+                c_view.getInstruLabel().setForeground(Color.RED);
             }
         }
     }
